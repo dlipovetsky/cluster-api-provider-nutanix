a1b5da79dc842cb3f5be5120dfed6034f9fa4813
Daniel Lipovetsky <daniel.lipovetsky@gmail.com>
2024-01-30 14:17:42 -0800

    Update script to run capx in a container

--- hackathon/local-provider-capx.sh
+++ hackathon/local-provider-capx.sh
@@ -1,29 +1,14 @@
 #!/usr/bin/env bash
 
-# The user gets this information from Prism Central.
-NUTANIX_ENDPOINT                = "example.com"
-NUTANIX_PORT                    = "9440"
+CAPX_IMG="docker.io/dlipovetsky/cluster-api-provider-nutanix:1.0h"
 
-# The user gets this from a Prism Central account.
-NUTANIX_USERNAME                = "foo"
-NUTANIX_PASSWORD                = "bar"
+# We need to source this to use $CAPX_KUBECONFIG and $CAPX_NAMESPACE in the docker run command.
+source capx.env
 
-# Given the endpoint and port, the user can download this via browser or openssl.
-# We can also download by running openssl in the VM on behalf of the user.
-NUTANIX_ADDITIONAL_TRUST_BUNDLE = ""
-NUTANIX_INSECURE                = "" # Must be true if no trust bundle is provided, false otherwise. 
-
-# Categories can be left empty.
-NUTANIX_CATEGORIES              = ""
-
-# DKP Workspace where the user creates the cluster.
-# User will note this in the UI.
-# In the future, we might be able to derive this on behalf of the user.
-CAPX_NAMESPACE                  = ""
-
-# User will download this from the Management cluster using the UI or CLI.
-CAPX_KUBECONFIG                 = 
-
-./cluster-api-provider-nutanix \
-    -namespace="$CAPX_NAMESPACE" \
-    -kubeconfig="$CAPX_KUBECONFIG"
+docker run \
+    --name capx \
+    --volume "$CAPX_KUBECONFIG:/kubeconfig" \
+    --env-file capx.env \
+    "$CAPX_IMG" \
+        -namespace="$CAPX_NAMESPACE" \
+        -kubeconfig="/kubeconfig"
